7fff07371d7bfd23611db3f88d335e1566379392
gyuha <nicegyuha@gmail.com>
2024-08-29 09:51:11 +0900

    vscode용 설정

--- app/utils/image_to_pdf_converter.py
+++ app/utils/image_to_pdf_converter.py
@@ -1,8 +1,7 @@
 import os
-
 from PIL import Image
 from PySide6.QtCore import QThread, Signal
-from reportlab.lib.pagesizes import letter
+from reportlab.lib.units import inch
 from reportlab.pdfgen import canvas
 
 
@@ -16,30 +15,25 @@ class ImageToPdfConverter(QThread):
         self.output_pdf = output_pdf
 
     def run(self):
-        image_files = [
-            f
-            for f in os.listdir(self.image_folder)
-            if f.endswith(("png", "jpg", "jpeg"))
-        ]
+        image_files = [f for f in os.listdir(self.image_folder) if f.lower().endswith(("png", "jpg", "jpeg"))]
         image_files.sort()
 
-        c = canvas.Canvas(self.output_pdf, pagesize=letter)
-        width, height = letter
-
+        c = canvas.Canvas(self.output_pdf)
         total_images = len(image_files)
+
         for i, image_file in enumerate(image_files):
             image_path = os.path.join(self.image_folder, image_file)
             img = Image.open(image_path)
 
-            img_width, img_height = img.size
-            aspect = img_height / float(img_width)
-            img = img.resize(
-                (int(width), int(width * aspect)), Image.Resampling.LANCZOS
-            )
+            # 이미지 크기를 인치 단위로 변환 (72 DPI 기준)
+            width_inch = img.width / 72.0
+            height_inch = img.height / 72.0
 
-            c.drawImage(
-                image_path, 0, height - img.height, width=img.width, height=img.height
-            )
+            # 페이지 크기를 이미지 크기로 설정
+            c.setPageSize((width_inch * inch, height_inch * inch))
+
+            # 이미지를 페이지에 그리기
+            c.drawImage(image_path, 0, 0, width=width_inch * inch, height=height_inch * inch)
             c.showPage()
 
             # 진행 상태 업데이트
