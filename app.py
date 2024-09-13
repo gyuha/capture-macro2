1faa1e04d8f3fe3a84205fb402b8804259a238ef
gyuha <nicegyuha@gmail.com>
2024-09-13 23:21:49 +0900

    update python version

--- app.py
+++ app.py
@@ -1,7 +1,6 @@
 import sys
 from multiprocessing import freeze_support
 
-# import qdarktheme
 from PySide6.QtGui import QIcon
 from PySide6.QtWidgets import QApplication
 
@@ -12,15 +11,12 @@ from app.utils.platform_util import is_macos
 if __name__ == "__main__":
     freeze_support()
 
-    # qdarktheme.enable_hi_dpi()
     app = QApplication(sys.argv)
 
     if is_macos():
         checker = MacOSAccessibilityChecker(app)
         if not checker.check_accessibility():
             exit(1)
-
-    # qdarktheme.setup_theme("auto")
 
     app_icon = QIcon("resources/icon.png")
     app.setWindowIcon(app_icon)
